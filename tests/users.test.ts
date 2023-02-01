dbb66f13635ec6c724b318c59ccb8a941d285ed2
Victor Azevedo <josevcazevedo@gmail.com>
2023-02-01 09:49:44 -0300

    chorus: add first test for jest test

---
+++ tests/users.test.ts
@@ -0,0 +1,17 @@
+describe("Users tests", () => {
+  it("teste 1", () => {
+    const resultado = 4 + 5;
+
+    expect(resultado).toBe(9);
+  });
+  it("teste 2", () => {
+    const resultado = 4 + 5;
+
+    expect(resultado).toBe(9);
+  });
+  it("teste 3", () => {
+    const resultado = 4 + 5;
+
+    expect(resultado).toBe(9);
+  });
+});
